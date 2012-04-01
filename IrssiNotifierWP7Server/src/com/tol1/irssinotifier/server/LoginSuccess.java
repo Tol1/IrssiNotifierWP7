860f6f2c8a9e6ca937a13c3d1a6da5e88a07f30d
Tomi Nokkala <tol1@iki.fi>
2012-04-01 22:40:11 +0300

    Lisää logincheck success-sivulle

--- IrssiNotifierWP7Server/src/com/tol1/irssinotifier/server/LoginSuccess.java
+++ IrssiNotifierWP7Server/src/com/tol1/irssinotifier/server/LoginSuccess.java
@@ -7,13 +7,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+@SuppressWarnings("serial")
 public class LoginSuccess extends HttpServlet {
 
+	private static UserService userService = UserServiceFactory.getUserService();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		resp.getWriter().println("Login Successful");
-		resp.getWriter().close();
+		User user = userService.getCurrentUser();
+
+		if (user != null) {
+			resp.getWriter().println("Login Successful");
+		} else {
+			resp.sendRedirect("/client/login");
+		}
 	}
 
 }
