a4e70c4049f2cd94074ed3c8e66f8f920f5a17ce
Tomi Nokkala <tol1@iki.fi>
2012-05-24 17:35:40 +0300

    Disclaimer kirjautumiseen ja lisätietoa apitokenista kirjautumisen jälkeen

--- IrssiNotifier/Views/InitialView.xaml.cs
+++ IrssiNotifier/Views/InitialView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace IrssiNotifier.Views
@@ -14,9 +15,15 @@ namespace IrssiNotifier.Views
 
 		public Page FromPage { get; private set; }
 
-		private void RegisterButtonClick(object sender, System.Windows.RoutedEventArgs e)
+		private void RegisterButtonClick(object sender, RoutedEventArgs e)
 		{
-			FromPage.NavigationService.Navigate(new Uri("/Pages/LoginPage.xaml", UriKind.Relative));
+			var answer = MessageBox.Show(
+				"Sinut ohjataan Googlen kirjautumissivulle. Tälle sovellukselle ei välity tilitietosi eikä salasanasi. Google välittää tälle sovellukselle ainoastaan tiedon kirjautumisesta.",
+				"Tietosuoja", MessageBoxButton.OKCancel);
+			if(answer == MessageBoxResult.Yes)
+			{
+				FromPage.NavigationService.Navigate(new Uri("/Pages/LoginPage.xaml", UriKind.Relative));
+			}
 		}
 	}
 }
