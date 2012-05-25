6e438ef59bdfef1a718b019438ff675cbf29d4c3
Tomi Nokkala <tol1@iki.fi>
2012-05-25 17:59:34 +0300

    Asetusnäkymässä takaisinnavigointi loogisemmaksi

--- IrssiNotifier/Pages/SettingsPage.xaml.cs
+++ IrssiNotifier/Pages/SettingsPage.xaml.cs
@@ -9,6 +9,19 @@ namespace IrssiNotifier.Pages
 			InitializeComponent();
 		}
 
+		protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
+		{
+			if(contentBorder.Child == null || contentBorder.Child is SettingsView)
+			{
+				base.OnBackKeyPress(e);
+			}
+			else
+			{
+				contentBorder.Child = SettingsView.GetInstance();
+				e.Cancel = true;
+			}
+		}
+
 		protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
 		{
 			contentBorder.Child = null;
