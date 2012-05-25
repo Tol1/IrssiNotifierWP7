5efbdb28946ffc511af1ea20734c4e372a53b461
Tomi Nokkala <tol1@iki.fi>
2012-05-25 17:54:49 +0300

    5 sekunnin hyppäykset ponnahdusviesti-intervallislideriin

---
+++ IrssiNotifier/Components/DiscreteSlider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace IrssiNotifier.Components
+{
+	public class DiscreteSlider : Slider
+	{
+		private bool _busy;
+		private double _discreteValue;
+
+		protected override void OnValueChanged(double oldValue, double newValue)
+		{
+			if (!_busy)
+			{
+				_busy = true;
+				if (SmallChange.CompareTo(0) != 0)
+				{
+					var newDiscreteValue = (int) (Math.Round(newValue/SmallChange))*SmallChange;
+					if (newDiscreteValue.CompareTo(Value) != 0)
+					{
+						Value = newDiscreteValue;
+						base.OnValueChanged(_discreteValue, newDiscreteValue);
+						_discreteValue = newDiscreteValue;
+					}
+				}
+				else
+				{
+					base.OnValueChanged(oldValue, newValue);
+				}
+				_busy = false;
+			}
+		}
+	}
+}
