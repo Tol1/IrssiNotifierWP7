68145935dde806d163387ab2497d15643ac211fd
Tomi Nokkala <tomi.nokkala@ecom.fi>
2012-05-25 14:36:24 +0300

    Estetään asetussivun muistivuoto vapauttamalla contentborder sivulta poistuttaessa

--- IrssiNotifier/Pages/SettingsPage.xaml.cs
+++ IrssiNotifier/Pages/SettingsPage.xaml.cs
@@ -7,6 +7,15 @@ namespace IrssiNotifier.Pages
 		public SettingsPage()
 		{
 			InitializeComponent();
+		}
+
+		protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+		{
+			contentBorder.Child = null;
+		}
+
+		protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+		{
 			contentBorder.Child = SettingsView.GetInstance();
 		}
 	}
