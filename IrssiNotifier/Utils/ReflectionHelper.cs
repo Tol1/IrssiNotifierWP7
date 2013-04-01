467f2af8dac0d282c35a17536bdcdacf500962f3
Tomi Nokkala <tol1@iki.fi>
2013-04-01 14:00:08 +0300

    Helper methods for reflection, because to support both 7.5 and 7.8/8 tiles, reflection must be used

---
+++ IrssiNotifier/Utils/ReflectionHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Phone.Shell;
+
+namespace IrssiNotifier.Utils
+{
+	public class ReflectionHelper
+	{
+		public static void Create(Uri uri, ShellTileData tiledata, bool usewide)
+		{
+			var shellTileType = Type.GetType("Microsoft.Phone.Shell.ShellTile, Microsoft.Phone");
+			if (shellTileType != null)
+			{
+				var createmethod = shellTileType.GetMethod("Create", new[] {typeof (Uri), typeof (ShellTileData), typeof (bool)});
+				createmethod.Invoke(null, new object[] {uri, tiledata, usewide});
+			}
+		}
+
+		public static void SetProperty(object instance, string name, object value)
+		{
+			var setMethod = instance.GetType().GetProperty(name).GetSetMethod();
+			setMethod.Invoke(instance, new[] {value});
+		}
+	}
+}
