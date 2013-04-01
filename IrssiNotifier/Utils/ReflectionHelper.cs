8e4a66490a1be2a06908c414b7c6a7c81afbf4c1
Tomi Nokkala <tol1@iki.fi>
2013-04-01 21:26:12 +0300

    Quick and dirty methods to create new tiles via reflection

--- IrssiNotifier/Utils/ReflectionHelper.cs
+++ IrssiNotifier/Utils/ReflectionHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Media;
 using Microsoft.Phone.Shell;
 
 namespace IrssiNotifier.Utils
@@ -19,6 +20,51 @@ namespace IrssiNotifier.Utils
 		{
 			var setMethod = instance.GetType().GetProperty(name).GetSetMethod();
 			setMethod.Invoke(instance, new[] {value});
+		}
+
+		public static ShellTileData CreateIconicTileData(string title, Uri iconImageUri, Uri smallIconImageUri, 
+			string text1, string text2, string text3, int? count = null, Color? backGroundColor = null)
+		{
+			var tileDataType = Type.GetType("Microsoft.Phone.Shell.IconicTileData, Microsoft.Phone");
+			var iconicTileData = (ShellTileData)tileDataType.GetConstructor(new Type[] { }).Invoke(null);
+			SetProperty(iconicTileData, "Title", title);
+			SetProperty(iconicTileData, "IconImage", iconImageUri);
+			SetProperty(iconicTileData, "SmallIconImage", smallIconImageUri);
+			SetProperty(iconicTileData, "WideContent1", text1);
+			SetProperty(iconicTileData, "WideContent2", text2);
+			SetProperty(iconicTileData, "WideContent3", text3);
+			if (count.HasValue)
+			{
+				SetProperty(iconicTileData, "Count", count);
+			}
+			if (backGroundColor.HasValue)
+			{
+				SetProperty(iconicTileData, "BackgroundColor", backGroundColor); //Color.FromArgb(255, 200, 10, 30)
+			}
+			return iconicTileData;
+		}
+
+		public static ShellTileData CreateFlipTileData(string title, string backTitle, string backContent, Uri smallBackgroundImageUri, Uri backgroundImageUri,
+			Uri backBackgroundImageUri, int? count, string wideBackContent, Uri wideBackgroundImageUri, Uri wideBackBackgroundImageUri)
+		{
+			var flipTileDataType = Type.GetType("Microsoft.Phone.Shell.FlipTileData, Microsoft.Phone");
+			var flipTileData = (ShellTileData)flipTileDataType.GetConstructor(new Type[] { }).Invoke(null);
+
+			// Set the properties. 
+			SetProperty(flipTileData, "Title", title);
+			if(count.HasValue)
+			{
+				SetProperty(flipTileData, "Count", count);
+			}
+			SetProperty(flipTileData, "BackTitle", backTitle);
+			SetProperty(flipTileData, "BackContent", backContent);
+			SetProperty(flipTileData, "SmallBackgroundImage", smallBackgroundImageUri);
+			SetProperty(flipTileData, "BackgroundImage", backgroundImageUri);
+			SetProperty(flipTileData, "BackBackgroundImage", backBackgroundImageUri);
+			SetProperty(flipTileData, "WideBackgroundImage", wideBackgroundImageUri);
+			SetProperty(flipTileData, "WideBackBackgroundImage", wideBackBackgroundImageUri);
+			SetProperty(flipTileData, "WideBackContent", wideBackContent);
+			return flipTileData;
 		}
 	}
 }
